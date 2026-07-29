efef9173deb0220d678d4dd7e193026637e9e841
dapangyu-fish <dapangyu-fish@users.noreply.github.com>
2026-07-29 14:50:33 +0800

    chore: move Homebrew formula to tap

--- app/src/main.c
+++ app/src/main.c
@@ -64,7 +64,7 @@ main_scrcpy(int argc, char *argv[]) {
     // Banner on stdout, except under --json (stdout must stay pure JSON there)
     if (!args.opts.json) {
         printf("scrcpy " SCRCPY_VERSION
-               " <https://github.com/fish-dapangyu-dev/scrcpy>\n"
+               " <https://github.com/fish-dapangyu-dev/scrcpy-auto>\n"
                "  (fork of <https://github.com/Genymobile/scrcpy>)\n");
     }
 
